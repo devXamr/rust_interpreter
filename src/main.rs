540197a49096df167d379b0f844ba9db017f2c68
devXamr <devxamr@gmail.com>
2025-09-16 12:18:10 +0530

    naive type declaration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+
+type TokenType;
+
+struct Token {
+    
+}
+
+
+
 fn main() {
     println!("Hello, world!");
+
+
+    
+        let ILLEGAL = "ILLEGAL";
+        let EOF = "EOF";
+        
+
+        let IDENT = "IDENT";
+        let INT = "INT";
+
+
+        let ASSIGN = "=";
+        let ADD = "+";
+
+
+        let COMMA = ",";
+        let SEMICOLON = ";";
+
+
+        let LPAREN = "(";
+        let RPAREN = ")";
+        let LBRACE = "{";
+        let RBRACE = "}";
+        // Keywords
+        let FUNCTION = "FUNCTION";
+        let LET= "LET";
 }
